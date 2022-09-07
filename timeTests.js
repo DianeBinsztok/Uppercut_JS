1f6dc917dabbc4bf53075140a72f67261c950faa
Comptabilité <diane.binsztok@le-campus-numerique.fr>
2022-09-07 14:07:42 +0200

    feat: JSON dataset and test page

---
+++ timeTests.js
@@ -0,0 +1,9 @@
+let today =Date();
+let now = new Date(today);
+let formatNow = ""+ now.getHours() + "h" + now.getMinutes();
+//let test = ""+ new Date(today.getHours()).getMinutes();
+
+console.log("today =>  ", today);
+console.log("now =>  ",now);
+console.log("formatNow =>  ",formatNow);
+//console.log("test =>  ",test);
