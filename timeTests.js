cab13aecb283d4c2859251c3a383e17e721fa14a
Comptabilité <diane.binsztok@le-campus-numerique.fr>
2022-09-09 11:28:02 +0200

    fix: just a commit to be sure

--- timeTests.js
+++
@@ -1,9 +0,0 @@
-let today =Date();
-let now = new Date(today);
-let formatNow = ""+ now.getHours() + "h" + now.getMinutes();
-//let test = ""+ new Date(today.getHours()).getMinutes();
-
-console.log("today =>  ", today);
-console.log("now =>  ",now);
-console.log("formatNow =>  ",formatNow);
-//console.log("test =>  ",test);
