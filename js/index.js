cb864093a0344d205860928690c2edf817e69cda
Comptabilité <diane.binsztok@le-campus-numerique.fr>
2022-09-12 13:32:58 +0200

    Added a Procfile

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ console.log(">index.js>");
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
-const port = 4000;
+const PORT = 4000;
+if (PORT == null || PORT == "") {
+  PORT = 8000;
+}
 
 const router=require("./router");
 app.use(router);
@@ -11,4 +14,4 @@ app.use(express.static("public"));
 app.use(express.static("public/css"));
 
 
-app.listen(port, () => console.log(`listening to http://localhost:${port}/`));
+app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}/`));
